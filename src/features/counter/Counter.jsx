cf8e1779df0f1d8b2da9d4c438f4ccb5029ba508
Febin <febinj14@gmail.com>
2023-06-27 12:27:03 +0530

    first commit

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
+  return (
+    <section>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+
+
+console.log(counterReducer);
+export const store = configureStore({
+    
+     reducer: {
+          counter: counterReducer,
+          
+     }
+})
+
